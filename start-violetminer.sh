9548522383b96b0b3547e627e709e14703bef458
Andryut <aahuertasca@unal.edu.co>
2021-04-08 08:54:43 -0500

    void

---
+++ start-violetminer.sh
@@ -0,0 +1,2 @@
+#!bin/bash
+./violetminer --algorithm turtlecoin --pool minenice.newpool.pw:1154 --username "TRTLuyvdhaUMWwKr6Vb4pNbQbjwxEgyLCDJRGnyp2JFTdN4UxLstnjThyMx7BaPxykXeLa2Z2Pn3LWhfjEGQ7w3FZwFukwgiAFi@Han@x" --password "x"
